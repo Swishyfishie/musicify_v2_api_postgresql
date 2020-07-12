2d5ebc2bc284227d19ba0ad2249280b14d5e3ae4
Swishyfishie <pclandreiuk@gmail.com>
2020-07-12 11:12:30 +0100

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Admin.create(username: "admin", password_digest:"test")
+Admin.create(username: "admin2", password_digest:"test")
+
+Concert.create(date: '11-Dec-2021', time: "18:00", event:"Charitable Concert", location:"The Super Arena", more_info:"Buy tickets from here", available:true)
+Concert.create(date: '21-Dec-2021', time: "19:00", event:"Private Event", location:"Private Location", more_info:"You can't really buy tickets", available:false)
