07f6de55d1e1e3443b8d3ec7d40323f02f49eeb6
Swishyfishie <pclandreiuk@gmail.com>
2020-07-12 11:08:32 +0100

    change routes + migrate admins table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :admins
-  resources :concerts
+  resources :admins, only: [:index, :show]
+  resources :concerts, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
